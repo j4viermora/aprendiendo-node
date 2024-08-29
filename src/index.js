293875411e177eea0aa55f9461e54d10c08cc81c
Javier A Mora gil <j4viermora@gmail.com>
2024-08-29 16:57:16 -0300

    completamos el crud

--- src/index.js
+++ src/index.js
@@ -11,28 +11,36 @@
 // Delete
 
 const ticketsService = () => {
-    console.log("body del a principal");
-
     let db = [];
 
     const create = (data) => {
-        db.push(data)
+        db.push(data);
     };
 
-    const read = () => {
-        console.log("body del read");
+    const read = (id) => {
+        return db.find((ticket) => ticket.id === id);
     };
 
     const list = () => {
-       return db
+        return db;
     };
 
-    const update = () => {
-        console.log("body del update");
+    const update = (id, name) => {
+        const newDB  = db.map((ticket) => {
+            if (ticket.id === id) {
+                ticket.owner = name;
+            }
+            return ticket;
+        })
+
+        db = [...newDB];
+        return db;
     };
 
-    const remove = () => {
-        console.log("body del delete");
+    const remove = (id) => {
+        const newDB = db.filter((ticket) => ticket.id !== id);
+        db = [...newDB];
+        return db;
     };
 
     return {
@@ -46,24 +54,36 @@ const ticketsService = () => {
 
 const tickets = ticketsService();
 const main = () => {
-    
-    const ticketsList = tickets.list();
-    console.log(ticketsList, "primera consulta")
-    
+    // console.log(ticketsList, "primera consulta")
+
     tickets.create({
-        "id": 1,
-        "owner": "Diego",
-        "event_name": "Cine",
-        "event_date": "2022-11-11"
+        id: 1,
+        owner: "Diego",
+        event_name: "Cine",
+        event_date: "2022-11-11",
+    });
+    tickets.create({
+        id: 2,
+        owner: "Diego",
+        event_name: "Cine",
+        event_date: "2022-11-11",
     });
 
-    console.log(ticketsList, "segunda consulta")
+    // console.log(ticketsList, "primera consulta lista de eventos");
 
-}
-main()
+    // obtener un ticket
 
+    // const getOne = ticketsList.find((ticket) => ticket.id === 2);
 
+    // console.log(getOne)
+    // console.log(tickets.remove(2), "first");
+    console.log(tickets.list(), "first");
+    // update one ticket
+    tickets.update(1, "Brian Luzardo");
+    console.log(tickets.list(), "updated");
 
+};
+main();
 
 // Ticket entidad - entity
 // id - unico
